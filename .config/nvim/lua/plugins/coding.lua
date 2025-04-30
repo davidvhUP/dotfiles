ffa42f64be0d6f8795018190dbae32b926cd55b8
davidvhUP <p4ndr3d@gmail.com>
2025-04-30 14:31:51 +0200

    added nvim repl

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -78,26 +78,19 @@ return{
 		require('jupytext').setup()
 	end,
 	},
-	{ "GCBallesteros/NotebookNavigator.nvim",
-	keys = {
-		{ "]h", function() require("notebook-navigator").move_cell "d" end },
-		{ "[h", function() require("notebook-navigator").move_cell "u" end },
-		-- { "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
-		-- { "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
-	},
-	dependencies = {
-		"echasnovski/mini.comment",
-		"hkupty/iron.nvim", -- repl provider
-		-- "akinsho/toggleterm.nvim", -- alternative repl provider
-		-- "benlubas/molten-nvim", -- alternative repl provider
-		"anuvyklack/hydra.nvim",
-	},
-	event = "VeryLazy",
-	config = function()
-		local nn = require "notebook-navigator"
-		nn.setup({ activate_hydra_keys = "<leader>h" })
-	end, 
-	},
+	
+	-- {
+	-- 	"pappasam/nvim-repl",
+	-- 	config = function()
+	-- 		require('repl').setup({
+	-- 			keys = {
+	-- 				{ "<Leader>c", "<Plug>(ReplSendCell)",   mode = "n", desc = "Send Repl Cell" },
+	-- 				{ "<Leader>r", "<Plug>(ReplSendLine)",   mode = "n", desc = "Send Repl Line" },
+	-- 				{ "<Leader>r", "<Plug>(ReplSendVisual)", mode = "x", desc = "Send Repl Visual Selection" },
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 
 	{ "Vigemus/iron.nvim",
 	config = function()
