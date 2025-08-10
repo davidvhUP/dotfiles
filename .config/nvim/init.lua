23210f5015e25089027336d6232f3f6eb46809fb
davidvhUP <u20510358@tuks.co.za>
2025-08-10 16:50:43 +0200

    added lua snippets

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,3 +29,17 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     command = "silent! loadview | filetype detect | set foldmethod=manual | normal! zM"
 })
 
+-- Lua snippets
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/luasnippets/" })
+-- Yes, we're just executing a bunch of Vimscript, but this is the officially
+-- endorsed method; see https://github.com/L3MON4D3/LuaSnip#keymaps
+vim.cmd[[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
