7bc59044d9486b1134042b3a167a52b8f9967d6e
davidvhUP <u20510358@tuks.co.za>
2026-02-03 20:50:59 +0200

    modified formatting options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,12 +8,15 @@ vim.opt.expandtab = false -- convert tabs to spaces
 vim.opt.autoindent = true -- auto indentation
 vim.opt.list = true -- show tab characters and trailing whitespace
 vim.opt.autochdir = true -- Automatically change dir to file dir
-vim.opt.textwidth = 100 -- Set text width for wrapping
+-- vim.opt.textwidth = 100 -- Set text width for wrapping
 vim.opt.wrap = true
 -- vim.opt.hlsearch = false -- don't higlight results from previous search
 vim.opt.ignorecase = true -- search ignores case
 vim.opt.undofile = true -- persistent undo (even when closing and opening files)
  
+-- set formatoptions
+vim.opt.formatoptions:remove("t")
+
 --colorscheme
 -- vim.cmd("colorscheme catppucin-macchiato")
 
